7ab8c609cdaaeae526721c528540bb87e3255077
Pauu10 <pauuu.10.03@gmail.com>
2014-11-25 16:01:55 -0300

    log

--- MVCDisco/MVCDisco/Controllers/HomeController.cs
+++ MVCDisco/MVCDisco/Controllers/HomeController.cs
@@ -3,19 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCDisco.Models;
+using MVCDisco.Servicios;
 
 namespace MVCDisco.Controllers
 {
     public class HomeController : Controller
     {
+        UsuarioServicio usuarioServicio = new UsuarioServicio();
+
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
-            ViewBag.Mensaje = "natalia";
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            Usuarios user = new Usuarios();
+
+            if (ModelState.IsValid)
+            {
+
+                user.Email = form["email"];
+                user.Contrasenia = form["contrasenia"];
+
+                if (usuarioServicio.usuarioValido(user.Email, user.Contrasenia))
+                {
+                    if (usuarioServicio.usuarioInactivo(user.Email, user.Contrasenia))
+                    {
+                        ViewBag.Text = "Usuario Inactivo";
+                        return View();
+                    }
+                    else
+                    {
+
+                        return View("Login");
+                    }
+                }
+                else
+                {
+                    ViewBag.Text = "usuario invalido";
+                    return View();
+                }
+
+            }
+
+            return View();
+        }
+
+
 
         //
         // GET: /Home/Details/5
--- MVCDisco/MVCDisco/Servicios/UsuarioServicio.cs
+++ MVCDisco/MVCDisco/Servicios/UsuarioServicio.cs
@@ -2,10 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using MVCDisco.Models;
 
 namespace MVCDisco.Servicios
 {
     public class UsuarioServicio
     {
+        TP20142CEntities1 db = new TP20142CEntities1();
+
+        public bool usuarioValido(string email, string contra)
+        {
+            var valido = (from u in db.Usuarios where u.Email == email && u.Contrasenia == contra select u).FirstOrDefault();
+
+            if (valido != null)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public bool usuarioInactivo(string email, string contra)
+        {
+
+            var inactivo = (from c in db.Usuarios
+                            where c.Email == email && c.Contrasenia == contra && c.Estado == 0
+                            select c).FirstOrDefault();
+
+            if (inactivo != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
